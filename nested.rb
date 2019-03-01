63a460215eb177df62d196a08d4ebc2408b6001a
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 19:10:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
      alans_new_info = "GUI"
      
     programmer_hash[:yukihiro_matsumoto] = {:known_for = "Ruby", :languages = ["LISP", "C"]}
-    return programmer_hash
+    programmer_hash
 
 end
 
