91dd8056c6342ba5906935817da8aa6da655f8c4
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 19:10:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-    programmer_hash[:yukihiro_matsumoto] = {:known_for] = "Ruby", :languages = ["LISP", "C"]}
+    programmer_hash[:yukihiro_matsumoto] = {:known_for = "Ruby", :languages = ["LISP", "C"]}
     return programmer_hash
 
 end
