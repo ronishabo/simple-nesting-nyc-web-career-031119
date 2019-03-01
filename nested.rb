52a6928a0d7dd813b3f6ba64b44085e8c884a0fa
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 19:15:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie] = 
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
 
 
